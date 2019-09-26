0e5303016a0c1f346c251afeca88842e86a3e2df
unknown <athula1wasala@gmail.com>
2019-09-26 06:47:11 +0530

    added old thing

---
+++ app/Listeners/RevokeExistingTokens.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\DB;
+
+class RevokeExistingTokens {
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct() {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event) {
+       
+        
+        DB::table('oauth_access_tokens')
+            ->where('id', '<>', $event->tokenId)
+            ->where('user_id', $event->userId)
+            ->where('client_id', $event->clientId)
+            ->update(['revoked' => true]);
+       
+        
+    }
+
+}
